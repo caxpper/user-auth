6f1bc5b031cd17716b85b8fe189e1048fc4fcce4
Alejandro Gaspar <caxpper@hotmail.com>
2018-04-13 18:13:56 -0700

    create an account Completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,78 @@
 import types from './types';
+import axios from 'axios';
 
-export function changeAuth(isAuth){
-    return {
-        type: types.CHANGE_AUTH,
-        payload: isAuth
+export function signUp(cred){
+    return (dispatch) =>{
+        axios.post('http://api.reactprototypes.com/signup',cred).then(resp => {
+            console.log('Sign Up Response:',resp);
+            localStorage.setItem('token',resp.data.token);
+            
+            dispatch({
+                type: types.SIGN_UP
+            });
+        });
     }
+}
+
+export function signIn(cred){
+    return (dispatch) =>{
+        console.log('Sign In called in:',cred);
+        axios.post('http://api.reactprototypes.com/signin',cred).then(resp => {
+            console.log('Sign In Response:',resp);
+            localStorage.setItem('token',resp.data.token);
+
+            dispatch({
+                type: types.SIGN_IN
+            });
+        });
+    }
+}
+
+export function signOut(){
+
+    localStorage.removeItem('token');
+
+    return {
+       type: types.SIGN_OUT
+    }
+}
+
+export function getQuote(){
+    return async (dispatch) => {
+        const axiosConfig = {
+            headers: {
+                authorization: localStorage.getItem('token')
+            }
+        };
+
+        try{
+            const response = await axios.get('http://api.reactprototypes.com',axiosConfig);
+
+            dispatch({
+                type: types.GET_QUOTE,
+                payload: response.data.message
+            });
+        }catch(err){
+            console.error('Error:',err.message);
+        }
+       
+    }
+}
+
+export function handleInputChange(event){
+    event.preventDefault();
+    const {name,value} = event.target;
+    return {
+        type: types.INPUT_CHANGE,
+        name,
+        value
+    };
+}
+
+export function formError(error){
+    
+    return {
+        type: types.FORM_ERROR,
+        error
+    };
 }
